a6f4618c381d7a21e5ef1134dbf285d081dcba15
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-05-13 11:04:20 +0700

    remove style-up function, just the core

--- filedrag.js
+++ filedrag.js
@@ -75,26 +75,26 @@ Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
 	// initialize
 	function Init() {
 
-		var fileselect = $id("fileselect"),
-			filedrag = $id("filedrag"),
-			submitbutton = $id("submitbutton");
+		// var fileselect = $id("fileselect");
+		var	filedrag = $id("filedrag");
+		// var	submitbutton = $id("submitbutton");
 
 		// file select
-		fileselect.addEventListener("change", FileSelectHandler, false);
+		// fileselect.addEventListener("change", FileSelectHandler, false);
 
 		// is XHR2 available?
-		var xhr = new XMLHttpRequest();
-		if (xhr.upload) {
+		// var xhr = new XMLHttpRequest();
+		// if (xhr.upload) {
 
 			// file drop
 			filedrag.addEventListener("dragover", FileDragHover, false);
-			filedrag.addEventListener("dragleave", FileDragHover, false);
+			// filedrag.addEventListener("dragleave", FileDragHover, false);
 			filedrag.addEventListener("drop", FileSelectHandler, false);
 			filedrag.style.display = "block";
 
 			// remove submit button
-			submitbutton.style.display = "none";
-		}
+			// submitbutton.style.display = "none";
+		// }
 
 	}
 
